6bd21bc3569e057925e671c6888b19efa189fdd2
ngotokodawari <naoya.goto@kodawari-japan.co.jp>
2022-07-22 10:29:58 +0900

    onclickでイベント呼ぶ

--- App.tsx
+++ App.tsx
@@ -7,13 +7,20 @@ export default function App(props) {
   return (
     <div className="box-containar">
       <Tabs>
-        <Tab title="初期画面" visible="true"></Tab>
-        <Tab title="その他" visible="true"></Tab>
-        <Tab title="非表示" visible="false"></Tab>
+        <Tab title="初期画面" visible="true" onclick={handleSubmit}></Tab>
+        <Tab title="その他" visible="true" onclick={handleSubmit}></Tab>
+        <Tab title="非表示" visible="false" onclick={handleSubmit}></Tab>
       </Tabs>
-      <div className="box" style={{ alignSelf: 'flex-end' }}>
-      <a href="https://www.yahoo.co.jp/" target="_blank" rel="noopener">help</a>
+      <div className="box-right" style={{ alignSelf: 'flex-end' }}>
+        <a href="https://www.yahoo.co.jp/" target="_blank" rel="noopener">
+          help
+        </a>
       </div>
     </div>
   );
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log('You clicked submit.');
 }
